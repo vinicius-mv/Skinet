2caa9f2c72618e360bfc1874a097dd99d092e593
Vinícius Vieira <viniciusmv91@gmail.com>
2025-03-23 14:16:12 -0300

    Implement ProductRepository and IProductRepository for product data access

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,6 +11,7 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,20 @@
+using Core.Entities;
+
+namespace Core.Interfaces;
+
+public interface IProductRepository
+{
+    Task<IReadOnlyList<Product>> GetProductsAsync();
+
+    Task<Product?> GetProductByIdAsync(int id);
+
+    void AddProduct(Product product);
+
+    void UpdateProduct(Product product);
+
+    void DeleteProduct(Product product);
+
+    Task<bool> ProductExists(int id);
+
+    Task<bool> SaveChangesAsync();
+}
