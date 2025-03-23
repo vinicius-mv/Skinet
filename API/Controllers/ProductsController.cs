d49812d86040757156bed330092054c9913e3b3b
Vinícius Vieira <viniciusmv91@gmail.com>
2025-03-23 12:01:47 -0300

    Add update and delete operations to ProductsController

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -42,5 +42,36 @@ namespace API.Controllers
             return product;
         }
 
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> UpdateProduct(int id, Product product)
+        {
+            if (product.Id != id || !ProductExists(id))
+                return BadRequest("Cannot update this product");
+
+            context.Entry(product).State = EntityState.Modified;
+
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var product = await context.Products.FindAsync(id);
+
+            if (product == null) return NotFound();
+
+            context.Products.Remove(product);
+
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductExists(int id)
+        {
+            return context.Products.Any(x => x.Id == id);
+        }
     }
 }
