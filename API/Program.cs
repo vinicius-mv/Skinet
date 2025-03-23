72044e91b4e1fed7dc1e3a1e6614efe972338f04
Vinícius Vieira <viniciusmv91@gmail.com>
2025-03-23 00:05:22 -0300

    Add Entity Framework Core support and configure StoreContext for database access

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,15 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
