b990da3273285c7cbbc3e4f4fae2309480cbcf0d
Vinicius Vieira <viniciusmv91@gmail.com>
2025-03-28 14:26:48 -0300

    Add CORS support to allow requests from specified origins

--- API/Program.cs
+++ API/Program.cs
@@ -14,11 +14,14 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 });
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
