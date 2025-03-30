dc21ac6b7c24d5e40c5a39ddf001ecd627ba7610
Vinícius Vieira <viniciusmv91@gmail.com>
2025-03-30 18:14:35 -0300

    Add product details view; implement product retrieval and display in product-details component

--- client/src/app/features/shop/product-item/product-item.component.ts
+++ client/src/app/features/shop/product-item/product-item.component.ts
@@ -4,6 +4,7 @@ import { MatCardModule } from '@angular/material/card';
 import { CurrencyPipe } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-item',
@@ -11,7 +12,8 @@ import { MatIconModule } from '@angular/material/icon';
     MatCardModule,
     CurrencyPipe,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    RouterLink
   ],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss'
