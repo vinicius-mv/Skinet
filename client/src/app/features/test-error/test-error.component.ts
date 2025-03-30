2be3fba658fa1a80d95a1c40af1bd9d1e9a6ce35
Vinícius Vieira <viniciusmv91@gmail.com>
2025-03-30 18:54:27 -0300

    Add test error component; implement error simulation buttons and update routing

---
+++ client/src/app/features/test-error/test-error.component.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-test-error',
+  imports: [
+    MatButtonModule,
+  ],
+  templateUrl: './test-error.component.html',
+  styleUrl: './test-error.component.scss'
+})
+export class TestErrorComponent {
+  baseUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
+
+  get404Error() {
+    this.http.get(this.baseUrl + 'buggy/notfound').subscribe({
+      next: response => console.log(response),
+      error: err => console.log(err)
+    });
+  }
+
+  get400Error() {
+    this.http.get(this.baseUrl + 'buggy/badrequest').subscribe({
+      next: response => console.log(response),
+      error: err => console.log(err)
+    });
+  }
+
+  get401Error() {
+    this.http.get(this.baseUrl + 'buggy/unauthorized').subscribe({
+      next: response => console.log(response),
+      error: err => console.log(err)
+    });
+  }
+
+  get500Error() {
+    this.http.get(this.baseUrl + 'buggy/internalerror').subscribe({
+      next: response => console.log(response),
+      error: err => console.log(err)
+    });
+  }
+
+  get400ValidationError() {
+    this.http.post(this.baseUrl + 'buggy/validationerror', {}).subscribe({
+      next: response => console.log(response),
+      error: err => console.log(err)
+    });
+  }
+}
