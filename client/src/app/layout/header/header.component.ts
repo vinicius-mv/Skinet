90ef25e3674377c8e222a44a1a0eabb5ad15086e
Vinicius Vieira <viniciusmv91@gmail.com>
2025-03-30 22:16:35 -0300

    Add loading indicator to header component using BusyService and MatProgressBar

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,21 +1,24 @@
-import { Component } from '@angular/core';
-import { MatBadge } from '@angular/material/badge';
-import { MatButton } from '@angular/material/button';
-import { MatIcon } from '@angular/material/icon';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { MatBadge, MatBadgeModule } from '@angular/material/badge';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
+import { BusyService } from '../../core/services/busy.service';
 
 @Component({
   selector: 'app-header',
   imports: [
-    MatIcon,
-    MatButton,
-    MatBadge,
-    RouterLink,
-    RouterLinkActive
+    MatIconModule,
+    MatButtonModule,
+    MatBadgeModule,
+    RouterModule,
+    RouterLinkActive,
+    MatProgressBarModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  busyService = inject(BusyService);
 }
