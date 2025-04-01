f46eb75b944461e9b0c02d7196f779c85cf541d0
Vinícius Vieira <viniciusmv91@gmail.com>
2025-03-31 21:06:11 -0300

    Implement item removal and quantity adjustment in CartService and update cart item component

--- client/src/app/core/services/cart.service.ts
+++ client/src/app/core/services/cart.service.ts
@@ -56,6 +56,34 @@ export class CartService {
     this.setCart(cart);
   }
 
+  removeItemFromCart(productId: number, quantity = 1) {
+    const cart = this.cart();
+    if (!cart) return;
+    const index = cart.items.findIndex(x => x.productId === productId);
+    if (index !== -1) {
+      if (cart.items[index].quantity > quantity) {
+        cart.items[index].quantity -= quantity;
+      } else { // remove item from cart
+        cart.items.splice(index, 1);
+      }
+
+      if (cart.items.length === 0) {
+        this.deleteCart();
+      } else {
+        this.setCart(cart);
+      }
+    }
+  }
+
+  deleteCart() {
+    this.http.delete(this.baseUrl + 'cart?id=' + this.cart()?.id).subscribe({
+      next: () => {
+        localStorage.removeItem('cart_id');
+        this.cart.set(null);
+      }
+    });
+  }
+
   private addOrUpdateItem(items: CartItem[], item: CartItem, quantity: number): CartItem[] {
     const index = items.findIndex(x => x.productId === item.productId);
     if (index === -1) {
--- client/src/app/features/cart/cart-item/cart-item.component.ts
+++ client/src/app/features/cart/cart-item/cart-item.component.ts
@@ -1,9 +1,10 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { CartItem } from '../../../shared/models/cart';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule, MatIconButton } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { CartService } from '../../../core/services/cart.service';
 
 @Component({
   selector: 'app-cart-item',
@@ -18,4 +19,17 @@ import { CommonModule } from '@angular/common';
 })
 export class CartItemComponent {
   item = input.required<CartItem>();
+  cartService = inject(CartService);
+
+  incrementQuantity() {
+    this.cartService.addItemToCart(this.item(), 1);
+  }
+
+  decrementQuantity() {
+    this.cartService.removeItemFromCart(this.item().productId, 1);
+  }
+
+  removeItemFromCart() {
+    this.cartService.removeItemFromCart(this.item().productId, this.item().quantity);
+  }
 }
