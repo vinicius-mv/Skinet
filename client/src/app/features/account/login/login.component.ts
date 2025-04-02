b4151b2e5c729e8d0faa572bbe701ad8c8e001c2
Vinicius Vieira <viniciusmv91@gmail.com>
2025-04-02 00:35:23 -0300

    Implement login form with reactive validation and enhance account service for credential handling

--- client/src/app/features/account/login/login.component.ts
+++ client/src/app/features/account/login/login.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { AccountService } from '../../../core/services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButton
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  private fb = inject(FormBuilder);
+  private accountService = inject(AccountService);
+  private router = inject(Router);
 
+  loginForm = this.fb.group({
+    email: [''],
+    password: [''],
+  });
+
+  onSubmit() {
+    this.accountService.login(this.loginForm.value).subscribe({
+      next: () => {
+        this.accountService.getUserInfo();
+        this.router.navigateByUrl('/shop');
+      }
+    })
+  }
 }
