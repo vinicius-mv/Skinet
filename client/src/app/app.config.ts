5fa3d56533bef92480d47bf0159a71f5225c1350
Vinicius Vieira <viniciusmv91@gmail.com>
2025-04-02 14:31:26 -0300

    Add auth interceptor to set requests to send cookies

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -8,13 +8,18 @@ import { errorInterceptor } from './core/interceptors/error.interceptor';
 import { loadingInterceptor } from './core/interceptors/loading.interceptor';
 import { InitService } from './core/services/init.service';
 import { lastValueFrom } from 'rxjs';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor])),
+    provideHttpClient(withInterceptors([
+      errorInterceptor,
+      loadingInterceptor,
+      authInterceptor
+    ])),
     provideAppInitializer(initializeApp), // Provide the initialization function directly
   ],
 };
--- client/src/app/core/services/account.service.ts
+++ client/src/app/core/services/account.service.ts
@@ -15,7 +15,7 @@ export class AccountService {
   login(values: any) {
     let params = new HttpParams();
     params = params.append('useCookies', true);
-    return this.http.post<User>(this.baseUrl + 'login', values, { params, withCredentials: true });
+    return this.http.post<User>(this.baseUrl + 'login', values, { params });
   }
 
   register(values: any) {
@@ -23,7 +23,7 @@ export class AccountService {
   }
 
   getUserInfo() {
-    return this.http.get<User>(this.baseUrl + 'account/user-info', { withCredentials: true }).pipe(
+    return this.http.get<User>(this.baseUrl + 'account/user-info').pipe(
       map(user => {
         this.currentUser.set(user);
         return user;
@@ -32,7 +32,7 @@ export class AccountService {
   }
 
   logout() {
-    return this.http.post(this.baseUrl + 'account/logout', {}, { withCredentials: true });
+    return this.http.post(this.baseUrl + 'account/logout', {});
   }
 
   updateAddress(address: Address) {
