453dafe596066570be0a8b06b75a7077880851f1
Vinicius Vieira <viniciusmv91@gmail.com>
2025-04-02 14:16:13 -0300

    Refactor account service to persist user login and update init service to fetch user data on app initialization

--- client/src/app/core/services/init.service.ts
+++ client/src/app/core/services/init.service.ts
@@ -1,12 +1,14 @@
 import { inject, Injectable } from '@angular/core';
 import { CartService } from './cart.service';
-import { of } from 'rxjs';
+import { forkJoin, of } from 'rxjs';
+import { AccountService } from './account.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InitService {
   private cartService = inject(CartService);
+  private accountService = inject(AccountService);
 
   init() {
     // get the cart from database on app initialization
@@ -14,7 +16,12 @@ export class InitService {
 
     // app initializer works does not work with signals or subscription, so we need observables for this function
     const cart$ = cartId ? this.cartService.getCart(cartId) : of(null);
+    const user$ = this.accountService.getUserInfo();
 
-    return cart$;
+    // forJoin allow us to wait for multiple observables to complete and emit the result array
+    return forkJoin({
+      cart: cart$,
+      user: user$
+    });
   }
 }
